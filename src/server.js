c18968a501163fbd7d0b47a08bf88a9bf16d54cb
Alcal <alcal.rs221@gmail.com>
2023-06-27 10:54:16 +0200

    test server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,19 @@
-console.log('hello')
+import fastify from "fastify";
+
+const fast = fastify({
+    logger: true
+})
+
+fast.get('/', async (request, reply) => {
+    return { hello: 'world' }
+  })
+
+  const start = async () => {
+    try {
+      await fast.listen({ port: 3000 })
+    } catch (err) {
+      fast.log.error(err)
+      process.exit(1)
+    }
+  }
+  start()
