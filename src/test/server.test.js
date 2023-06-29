9f53ae71c42786b5a671e4dbff4a2a5ae0c1a4a2
Alcal <alcal.rs221@gmail.com>
2023-06-29 16:10:21 +0200

    Test POST/todos

--- src/test/server.test.js
+++ src/test/server.test.js
@@ -16,4 +16,17 @@ describe('Test Todos', () => {
         expect(response.body).toMatchObject(todos)
         expect(response.body).toEqual(expect.arrayContaining(expected))
     })
+    it('Test POST/todos voir si on créé bien une todo', async() => {
+        const newTodo = {
+            "title": 'Test Todo',
+            "completed": false
+        }
+        const response = await request(baseUrl).post('/todos').send(newTodo)
+        expect(response.statusCode).toBe(200)
+        expect(response.body.id).toBeDefined()
+        expect(response.body.title).toBe(newTodo.title)
+        expect(response.body.completed).toBe(newTodo.completed)
+        console.log(response.body)
+    })
+    
 })
